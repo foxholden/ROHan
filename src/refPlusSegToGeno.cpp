8bfff93b6f7157129f2dd76602a2cf9bb122713f
grenaud <gabriel.reno@gmail.com>
2016-04-15 23:26:45 +0200

    add

---
+++ src/refPlusSegToGeno.cpp
@@ -0,0 +1,113 @@
+/*
+ * refPlusSegToGeno
+ * Date: Apr-13-2016 
+ * Author : Gabriel Renaud gabriel.reno [at sign here ] gmail.com
+ *
+ */
+
+#include <iostream>
+#include <fstream>
+#include <gzstream.h>
+
+#include "utils.h"
+
+using namespace std;
+
+typedef struct{
+    string ref;
+    unsigned int coordinate;
+    char al1;
+    char al2;
+} segsite;
+
+int main (int argc, char *argv[]) {
+
+
+   igzstream myFileSeg;
+   igzstream myFileRef;
+
+   string refFile  = string(argv[1]);
+   string segSites = string(argv[2]);
+
+   string lineRef;
+   string lineSeg;
+   
+   myFileRef.open(refFile.c_str(), ios::in);
+   myFileSeg.open(segSites.c_str(), ios::in);
+
+   segsite nextSS;
+
+   if (myFileSeg.good()){
+       if(getline (myFileSeg,lineSeg)){
+	   vector<string> token=allTokens(lineSeg,'\t');
+			 
+	   nextSS.ref        =                      token[0];
+	   nextSS.coordinate = destringify<unsigned int>(token[1]);
+	   nextSS.al1        = destringify<char>(        token[2]);
+	   nextSS.al2        = destringify<char>(        token[3]);
+       }else{
+	   nextSS.ref        =                      "-1";
+       }
+       
+   }else{
+       cerr << "Unable to open file "<<segSites<<endl;
+       return 1;
+    }
+
+
+   //   return 1;
+
+   string currentRef="-1";
+   unsigned int coordinate=0;
+
+   if (myFileRef.good()){
+     while ( getline (myFileRef,lineRef)){
+	 //cout<<lineRef<<endl;
+	 if(lineRef[0] == '>'){	     
+	     currentRef=lineRef;
+	     coordinate=0;
+	 }else{
+	     for(unsigned int i=0;i<lineRef.size();i++){
+		 coordinate++;
+
+		 if(currentRef == nextSS.ref 
+		    &&
+		    coordinate == nextSS.coordinate ){
+		     if(lineRef[i] != nextSS.al1 &&
+			lineRef[i] != nextSS.al2 ){
+			 cerr<<"internal error"<<endl;
+			 return 1;
+		     }	
+		     
+		     if(nextSS.al1<nextSS.al2)
+			 cout<<""<<currentRef<<"\t"<<coordinate<<"\t"<<nextSS.al1<<nextSS.al2<<endl;
+		     else
+			 cout<<""<<currentRef<<"\t"<<coordinate<<"\t"<<nextSS.al2<<nextSS.al1<<endl;
+		     
+		     if(getline (myFileSeg,lineSeg)){
+			 vector<string> token=allTokens(lineSeg,'\t');
+			 
+			 nextSS.ref        =                      token[0];
+			 nextSS.coordinate = destringify<unsigned int>(token[1]);
+			 nextSS.al1        = destringify<char>(        token[2]);
+			 nextSS.al2        = destringify<char>(        token[3]);
+		     }else{
+			 nextSS.ref        =                      "-1";
+		     }
+
+		 }else{
+		     cout<<currentRef<<"\t"<<coordinate<<"\t"<<lineRef[i]<<lineRef[i]<<endl;
+		 }
+
+	     }
+	     
+	 }
+     }
+     myFileRef.close();
+   }else{
+       cerr << "Unable to open file "<<refFile<<endl;
+       return 1;
+    }
+    return 0;
+}
+
---
+++ src/sitesPlusGeno.cpp
@@ -0,0 +1,141 @@
+/*
+ * refPlusSegToGeno
+ * Date: Apr-13-2016 
+ * Author : Gabriel Renaud gabriel.reno [at sign here ] gmail.com
+ *
+ */
+
+#include <iostream>
+#include <fstream>
+#include <gzstream.h>
+
+#include "utils.h"
+
+using namespace std;
+
+typedef struct{
+    string ref;
+    unsigned int coordinate;
+    char al1;
+    char al2;
+} segsite;
+
+typedef struct{
+    string ref;
+    unsigned int coordinate;
+    char al1;
+    char al2;
+    double qual;
+} geno;
+
+int main (int argc, char *argv[]) {
+
+
+    igzstream myFileGen;
+    igzstream myFileSeg;
+    
+
+    string genFile   = string(argv[1]);
+    string segSites  = string(argv[2]);
+
+    string lineGen;
+    string lineSeg;
+   
+    myFileGen.open(genFile.c_str(),  ios::in);
+    myFileSeg.open(segSites.c_str(), ios::in);
+
+
+
+
+
+    if (myFileGen.good()){
+	
+    }else{
+    	cerr << "Unable to open file "<<genFile<<endl;
+    	return 1;
+    }
+
+    if (myFileSeg.good()){
+
+    }else{
+	cerr << "Unable to open file "<<segSites<<endl;
+	return 1;
+    }
+
+
+
+    //   return 1;
+    getline (myFileGen,lineGen);//header 
+
+    string       currentRef = "-1";
+    //    unsigned int coordinate =    0;
+
+
+    while(getline (myFileGen,lineGen)){
+	vector<string> tokenG=allTokens(lineGen,'\t');
+	//cout<<"G"<<lineGen<<endl;
+	geno g;
+	g.ref        =                           tokenG[0];
+	g.coordinate = destringify<unsigned int>(tokenG[1]);
+	g.al1        =                           tokenG[7][0];
+	g.al2        =                           tokenG[7][1];
+	g.qual       = destringify<double>(      tokenG[8] );
+
+
+	while ( getline (myFileSeg,lineSeg)){
+	    //cout<<"S"<<lineSeg<<endl;
+	    vector<string> token=allTokens(lineSeg,'\t');
+
+	    segsite s;//truth
+	    s.ref        =                           token[0];
+	    s.coordinate = destringify<unsigned int>(token[1]);
+	    s.al1        =                           token[2][0];
+	    s.al2        =                           token[2][1];
+	    
+	    if( (">"+g.ref)        == s.ref && 
+		g.coordinate == s.coordinate){
+		if(s.al1 == g.al1 //ok
+		   &&
+		   s.al2 == g.al2 ){
+		    
+		    if(s.al1 == s.al2)//truth is homo
+			cout<<"CHOM2HOM\t"<<g.ref<<"\t"<<g.coordinate<<"\t"<<g.al1<<g.al2<<"\t"<<s.al1<<s.al2<<"\t"<<g.qual<<"\t"<<lineGen<<endl;
+		    else
+			cout<<"CHET2HET\t"<<g.ref<<"\t"<<g.coordinate<<"\t"<<g.al1<<g.al2<<"\t"<<s.al1<<s.al2<<"\t"<<g.qual<<"\t"<<lineGen<<endl;
+		    
+		}else{//error
+		    
+		    if(s.al1 == s.al2){//truth is homo
+			if(g.al1 == g.al2){//predicted is homo
+			    cout<<"WHOM2HOM\t"<<g.ref<<"\t"<<g.coordinate<<"\t"<<g.al1<<g.al2<<"\t"<<s.al1<<s.al2<<"\t"<<g.qual<<"\t"<<lineGen<<endl;
+			}else{
+			    cout<<"WHOM2HET\t"<<g.ref<<"\t"<<g.coordinate<<"\t"<<g.al1<<g.al2<<"\t"<<s.al1<<s.al2<<"\t"<<g.qual<<"\t"<<lineGen<<endl;
+			}
+		    }else{//truth is hetero
+
+			if(g.al1 == g.al2){//predicted is homo
+			    cout<<"WHET2HOM\t"<<g.ref<<"\t"<<g.coordinate<<"\t"<<g.al1<<g.al2<<"\t"<<s.al1<<s.al2<<"\t"<<g.qual<<"\t"<<lineGen<<endl;
+			}else{
+			    cout<<"WHET2HET\t"<<g.ref<<"\t"<<g.coordinate<<"\t"<<g.al1<<g.al2<<"\t"<<s.al1<<s.al2<<"\t"<<g.qual<<"\t"<<lineGen<<endl;
+			}
+			
+		    }
+		}
+		break;
+	    }
+		  
+	}
+
+    }
+	    //cout<<lineSeg<<endl;
+
+    if (myFileSeg.good()){
+	myFileSeg.close();
+    }else{
+	cerr << "Unable to open file "<<segSites<<endl;
+	return 1;
+    }
+
+    return 0;
+}
+
